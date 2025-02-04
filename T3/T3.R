5471fcbff0ecc47ac92934b50835074d4ad3b617
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2025-02-03 18:11:56 -0600

    Finished ej 2

--- T3/T3.R
+++ T3/T3.R
@@ -49,3 +49,31 @@ SigmaInv
 x1menosx2 <- matrix(c(14,4,-7,-12,-5), nrow = 1, byrow = TRUE)
 sqrt((x1menosx2 %*% SigmaInv) %*%  t(x1menosx2))
 
+#2
+## a
+Sigma <- matrix(c(
+  10, -2, 1, 0, 3,
+  -2, 9, -3, 4, 5,
+  1, -3, 15, 7, -2,
+  0, 4, 7, 20, 2,
+  3, 5, -2, 2, 5
+), nrow = 5, byrow = TRUE)
+Sigma_div_40 <- Sigma / 40
+Sigma_div_40
+
+pnorm(229,230,sqrt(0.375))
+
+
+## b
+mu <- matrix(c(100,95,230,400,86), nrow = 5, byrow = TRUE)
+a <- matrix(c(4,0,3,-1,0), nrow = 5, byrow = TRUE)
+a_transpuesta <- t(a)
+a_transpuesta %*% mu
+(a_transpuesta %*% Sigma_div_40) %*% a
+
+pnorm(687,mean = 690, sd = sqrt(7.425))
+
+## c
+xbarramenosmu <- matrix(c(-0.5,1,1,0,0.2), nrow = 1, byrow = TRUE)
+Sigma_div_40_inversa <- solve(Sigma_div_40)
+sqrt((xbarramenosmu %*% Sigma_div_40_inversa) %*%  t(xbarramenosmu))
