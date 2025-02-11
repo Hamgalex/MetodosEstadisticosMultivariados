57990444333c4948defd887c5cac698f1c5c940e
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2025-02-10 21:38:53 -0600

    Finished e4

--- T4/T4.R
+++ T4/T4.R
@@ -166,3 +166,134 @@ for (i in 1:p) {
 
 IC_inf
 IC_sup
+
+
+# 3
+
+df <- read_excel("C:/Users/hamga/Documents/maestria/2do/MEM/MetodosEstadisticosMultivariados/T4/datos_tarea4.xlsx", sheet=3)
+df
+
+mean_vector <- colMeans(df)
+cov_matrix <- cov(df)
+cov_matrix_inv <- solve(cov_matrix)
+
+mean_vector
+cov_matrix
+cov_matrix_inv
+
+mu_0 <- c(0.85, 0.79, 1.80, 1.70, 0.7, 0.7)
+
+T2 <- nrow(df) * t(mean_vector - mu_0) %*% cov_matrix_inv %*% (mean_vector - mu_0)
+T2
+
+n <- nrow(df) 
+p <- ncol(df)
+alpha <- 0.05 
+
+F_critical <- ((n - 1) * p) / (n - p) * qf(1 - alpha, p, n - p)
+F_critical
+
+statistic <- ((n - p) * T2) / ((n - 1) * p)
+statistic
+
+# B
+
+IC_diff_lower <- list() 
+IC_diff_upper <- list()  
+
+for(i in 1:(p-1)) {
+  for(j in (i+1):p) {
+    
+    diff <- mean_vector[i] - mean_vector[j]
+    
+    margin_error <- sqrt((p * (n - 1)) / (n * (n - p)) * F_critical * (cov_matrix[i, i] - 2 * cov_matrix[i, j] + cov_matrix[j, j]))
+    
+    pair_name <- paste("X", i, "-", "X", j, sep = "") 
+    
+    IC_diff_lower[[pair_name]] <- diff - margin_error  
+    IC_diff_upper[[pair_name]] <- diff + margin_error  
+  }
+}
+
+for(pair in names(IC_diff_lower)) {
+  cat("Intervalo de Confianza para la diferencia de medias entre ", pair, ":\n", sep="")
+  cat("Límite Inferior: ", IC_diff_lower[[pair]], "\n")
+  cat("Límite Superior: ", IC_diff_upper[[pair]], "\n\n")
+}
+
+
+# C
+
+mean_vector <- colMeans(df)
+cov_matrix <- cov(df)
+
+d2 <- mahalanobis(df, center = mean_vector, cov = cov_matrix)
+valChiCuadrada <- qchisq(ppoints(length(d2)), df = 6)
+
+qqplot(valChiCuadrada, d2,
+       xlab = "Cuantiles teóricos(chi-cuad)",
+       ylab = "cuantiles muestrales (d2M)")
+
+abline(0, 1, col = "red")  
+
+# 4
+
+# A
+
+df <- read_excel("C:/Users/hamga/Documents/maestria/2do/MEM/MetodosEstadisticosMultivariados/T4/datos_tarea4.xlsx", sheet=4)
+df
+
+mean_vector <- colMeans(df)
+cov_matrix <- cov(df)
+cov_matrix_inv <- solve(cov_matrix)
+
+mean_vector
+cov_matrix
+cov_matrix_inv
+
+n <- nrow(df) 
+p <- ncol(df)
+alpha <- 0.05 
+
+mu_1 <- c(527, 53, 25)
+mu_2 <- c(523, 55, 26)
+mu_3 <- c(525, 53, 26)
+
+T2_1 <- n * t(mean_vector - mu_1) %*% cov_matrix_inv %*% (mean_vector - mu_1)
+T2_2 <- n * t(mean_vector - mu_2) %*% cov_matrix_inv %*% (mean_vector - mu_2)
+T2_3 <- n * t(mean_vector - mu_3) %*% cov_matrix_inv %*% (mean_vector - mu_3)
+
+T2_1
+T2_2
+T2_3
+
+F_critical <- ((n - 1) * p) / (n - p) * qf(1 - alpha, p, n - p)
+F_critical
+
+statistic <- ((n - p) * T2) / ((n - 1) * p)
+statistic
+
+# B 
+IC_diff_lower <- list() 
+IC_diff_upper <- list()  
+
+for(i in 1:(p-1)) {
+  for(j in (i+1):p) {
+    
+    diff <- mean_vector[i] - mean_vector[j]
+    
+    margin_error <- sqrt((p * (n - 1)) / (n * (n - p)) * F_critical * (cov_matrix[i, i] - 2 * cov_matrix[i, j] + cov_matrix[j, j]))
+    
+    pair_name <- paste("X", i, "-", "X", j, sep = "") 
+    
+    IC_diff_lower[[pair_name]] <- diff - margin_error  
+    IC_diff_upper[[pair_name]] <- diff + margin_error  
+  }
+}
+
+for(pair in names(IC_diff_lower)) {
+  cat("Intervalo de Confianza para la diferencia de medias entre ", pair, ":\n", sep="")
+  cat("Límite Inferior: ", IC_diff_lower[[pair]], "\n")
+  cat("Límite Superior: ", IC_diff_upper[[pair]], "\n\n")
+}
+
